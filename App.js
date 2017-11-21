a6508864725005ef99b0b324cc6ad4c02c5f81b6
Luis Eduardo Hernández García <luiseduardo@MBP-de-Luis.lan>
2017-11-20 18:03:10 -0600

    Esta es mi app prro

---
+++ App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon } from 'native-base';
+
+export default class App extends Component {
+    render(){
+        return(
+            <Container>
+                <Header>
+                    <Left>
+                        <Button transparent>
+                            <Icon name='menu' />
+                        </Button>
+                    </Left>
+                    <Body>
+                        <Title>Header</Title>
+                    </Body>
+                    <Right />
+                </Header>
+                <Content>
+                    <View style={styles.container}>
+                        <Text>Nuevo Hogar</Text>
+                    </View>
+                </Content>
+                <Footer>
+                    <FooterTab>
+                        <Button full>
+                            <Text>Footer</Text>
+                        </Button>
+                    </FooterTab>
+                </Footer>
+            </Container>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+});
